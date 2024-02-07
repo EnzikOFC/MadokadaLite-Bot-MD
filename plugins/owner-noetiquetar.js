8389efa0d0d113f8b5006f894d8d378049ac79cc
EnzikOFC <159156257+EnzikOFC@users.noreply.github.com>
2024-02-07 17:13:35 -0300

    Update owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,4 +1,4 @@
-handler = async (m, {conn, text}) => {
+let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
 conn.reply(m.chat, `*[ 😈 ️] 𝒩𝒪 𝐸𝒯𝐼𝒬𝒰𝐸𝒯𝐸𝒮 𝒜 𝑀𝐼 𝒪𝒲𝒩𝐸𝑅 *` ,m, { contextInfo:{ externalAdReply: {title: '𝓜𝓪𝓭𝓸𝓴𝓪𝓛𝓲𝓽𝓮𝓑𝓸𝓽-𝓜𝓓', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/5491168758497'}}})
