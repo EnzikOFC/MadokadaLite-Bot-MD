67fc46447ade7fc0bf7c294b7e9fd630cffbc12c
EnzikOFC <159156257+EnzikOFC@users.noreply.github.com>
2024-02-07 17:16:35 -0300

    Update owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,7 +1,7 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `[ 😈 ️] 𝗡𝗢 𝗘𝗧𝗜𝗤𝗨𝗘𝗧𝗘𝗦 𝗔 𝗠𝗜 𝗢𝗪𝗡𝗘𝗥 ` ,m, { contextInfo:{ externalAdReply: {title: '𝓜𝓪𝓭𝓸𝓴𝓪𝓛𝓲𝓽𝓮𝓑𝓸𝓽-𝓜𝓓', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/5491168758497'}}})
+conn.reply(m.chat, `[ 😈 ️] 𝑵𝑶 𝑬𝑻𝑰𝑸𝑼𝑬𝑻𝑬𝑺 𝑨 𝑴𝑰 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 ` ,m, { contextInfo:{ externalAdReply: {title: '𝓜𝓪𝓭𝓸𝓴𝓪𝓛𝓲𝓽𝓮𝓑𝓸𝓽-𝓜𝓓', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/5491168758497'}}})
 }
 handler.customPrefix = /@5491168758497/i;
 handler.command = new RegExp();
